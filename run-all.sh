9a307b86a1c0799c45eeece0a6d1ffa70ae86ef3
Ed F <61842596+ezfezf@users.noreply.github.com>
2021-12-01 06:37:42 -0500

    Create run-all.sh
    initial revision

---
+++ run-all.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# this is a shell script (intended for bash on Linux)
+# to run each of the sample programs
+# Note that you may need to make this file executable (with "chmod +x run-all.sh")
+
+# make sure all prerequisites are met
+check_tools()
+{
+    which dart > /dev/null
+    if [ $? -ne 0 ] ; then
+	echo "Is the dart SDK installed?"
+	echo "(see http://dart.dev/get-dart)"
+	exit 1
+    fi
+}
+
+main()
+{
+    echo "checking environment"
+    check_tools
+    echo "running exercise 1"
+    dart run fetch-subway.dart
+    exit $?
+}
+
+# do it
+main
